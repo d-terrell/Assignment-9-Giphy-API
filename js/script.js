c4c381c8234373d55abb9d7dd4ae2fe3c1cd82be
dterrell <donnaterrell17@gmail.com>
2025-11-05 17:09:28 -0600

    Debugged final output issues

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 console.log("script.js loaded");
+
+let endpoint = ("https://api.giphy.com/v1/gifs/search?api_key=1TlONf8RYUKT0YLDiIncBEzmCpX7sKlG&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+console.log(endpoint);
+
+const gifContainer = document.querySelector("#gif-container");
+
+const button = document.querySelector("#fetch-gif-btn");
+
+async function getDogImage() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const images = data.data.map(gif => gif.images.original.url);
+
+    console.log(images);
+
+
+for (let i = 0; i < images.length; i++) {
+    gifContainer.innerHTML += `<img src="${images[i]}" class="col-3 mb-3">`;
+  }
+}
+
+button.addEventListener("click", getDogImage);
